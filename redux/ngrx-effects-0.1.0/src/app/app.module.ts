9a7603abaedef7ec06cd43b5fb5dedab1ea907f4
roque <rorts@knowmadmood.com>
2023-10-17 13:12:28 +0200

    proyecto effects sin effects

---
+++ redux/ngrx-effects-0.1.0/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+// Rutas
+import { AppRoutingModule } from './app-routing.module';
+
+// Modulos personalizados
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    SharedModule,
+    UsuariosModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
